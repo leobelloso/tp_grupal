4dd22bfaf7fde4d0f462e1f12abebd55cbd51964
ATerzano <aterzano@frd.utn.edu.ar>
2024-06-30 19:13:04 -0300

    añadi el otro metodo de DOM

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const card2 = document.querySelector('.item-2');
-
+const imagenHombre = document.getElementById("hombre_home");
 
 card2.addEventListener ('click', ()=> {
     card2.style.backgroundImage = 'url(img/hombre_cyberpunk.png)';
 });
-
+imagenHombre.addEventListener('click', ()=> {
+    imagenHombre.src = "./img/hombre.png";
+});
