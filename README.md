690bfb7a5085416e4cd70cb026c424f70f971841
ATerzano <aterzano@frd.utn.edu.ar>
2024-06-30 17:59:34 -0300

    version final. DOM

---
+++ README.md
@@ -0,0 +1,2 @@
+# TPGrupal
+tp grupal prog3. aterzano
---
+++ js/main.js
@@ -0,0 +1,7 @@
+const card2 = document.querySelector('.item-2');
+
+
+card2.addEventListener ('click', ()=> {
+    card2.style.backgroundImage = 'url(img/hombre_cyberpunk.png)';
+});
+
