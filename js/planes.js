65bf58fb31851f58fb0c4e85b39b6cb59d621ab4
leobelloso <leonardobelloso@gmail.com>
2024-07-03 15:12:25 -0300

    Correcciones de forma al documento JS de planes.

--- js/planes.js
+++ js/planes.js
@@ -6,7 +6,7 @@ const titulo3 = document.querySelector(".cambiarP3");
 /* El código cambia el texto y le agrega sombreado al texto nuevo cuando se pasa el mouse por encima, luego se restaura al texto original, sin sombreado. */
 titulo1.addEventListener('mouseenter', () => {
     titulo1.textContent = "Entretenimiento"; 
-    titulo1.style.textShadow = "1px 1px 8px rgba(73, 217, 244, 0.5)"; 
+    titulo1.style.textShadow = "5px 5px 15px rgba(73, 217, 244, 0.5)"; 
 });
 
 
@@ -18,7 +18,7 @@ titulo1.addEventListener('mouseleave', () => {
 
 titulo2.addEventListener('mouseenter', () => {
     titulo2.textContent = "Profesionalidad";
-    titulo2.style.textShadow = "1px 1px 8px rgba(73, 217, 244, 0.5)";
+    titulo2.style.textShadow = "5px 5px 15px rgba(73, 217, 244, 0.5)";
 });
 
 
@@ -30,7 +30,7 @@ titulo2.addEventListener('mouseleave', () => {
 
 titulo3.addEventListener('mouseenter', () => {
     titulo3.textContent = "Confiabilidad";
-    titulo3.style.textShadow = "1px 1px 8px rgba(73, 217, 244, 0.5)";
+    titulo3.style.textShadow = "5px 5px 15px rgba(73, 217, 244, 0.5)";
 });
 
 
