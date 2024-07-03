029ebd5189dd2d69982e99f113b92234c1ba0ccd
leobelloso <leonardobelloso@gmail.com>
2024-07-03 11:54:47 -0300

    Agregado JS que controla el DOM para planes. Modifica los títulos de los planes en H2 y les agrega un sombreado cuando se pasa el mouse por encima, luego los restaura.

--- js/main.js
+++ js/main.js
@@ -7,3 +7,35 @@ card2.addEventListener ('click', ()=> {
 imagenHombre.addEventListener('click', ()=> {
     imagenHombre.src = "./img/hombre.png";
 });
+
+
+/* ------------------------------------------- */
+
+
+
+
+
+
+
+
+
+
+/* LEO TESTING */
+
+/* const card2 = document.querySelector('.item-2');
+const imagenHombre = document.getElementById("hombre_home");
+
+card2.addEventListener ('mouseenter', ()=> {
+    card2.style.backgroundImage = 'none';
+    card2.textContent = "Hola MUNDO";
+    card2.style.fontSize = "50px";
+    card2.style.color = "blue";
+    card2.style.fontWeight = "bold";
+});
+
+card2.addEventListener('mouseleave', () => {
+
+    card2.style.backgroundImage = 'url(img/home_1.jpg)';
+    card2.textContent = "";
+
+}); */
